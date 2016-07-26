3bd52f7e506ebde6097040931e048f9a19639cc3
Luka Petrovic <lukapetrovicsi@gmail.com>
2016-07-26 14:03:49 +0200

    Added checking. Usage of lib has changed. Functions return true if successfully executed. Fully tested.

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
-function inherit (child, parnt) {
-  child.prototype = Object.create(parnt.prototype,{constructor:{
-    value: child,
-    enumerable: false,
-    configurable: false,
-    writable: false
-  }});
+function createLib(has,isFunction,isString){
+  'use strict';
+  function inherit (child, parnt) {
+    if (!isFunction(child)){
+      throw new Error(child + ' is not a function.'); 
+    }
+    if (!isFunction(parnt)){
+      throw new Error(parnt + ' is not a function.'); 
+    }
+    child.prototype = Object.create(parnt.prototype,{constructor:{
+      value: child,
+      enumerable: false,
+      configurable: false,
+      writable: false
+    }});
+    return true;
+  }
+
+  function attachMethod (chld, parnt, name) {
+    if (!isFunction(chld)){
+      throw new Error(chld + ' is not a function.'); 
+    }
+    if (!isFunction(parnt)){
+      throw new Error(parnt + ' is not a function.'); 
+    }
+    if (!isString(name)){
+      throw new Error(name + ' is not a string. Method name must be a string.');
+    }
+    if (!has(parnt.prototype,name)){
+      throw new Error('Parent class does not have method ' + name);
+    }
+    chld.prototype[name] = parnt.prototype[name];
+  }
+
+  function inheritMethods (child, parnt) {
+    var methods = Array.prototype.slice.call(arguments, 2);
+    if (!methods.length) throw new Error('Avoid calling this function with no methods to inherit');
+    methods.forEach(attachMethod.bind(null, child, parnt));
+    methods = null;
+    child = null;
+    parnt = null;
+    return true;
+  }
+
+  return {
+    inherit: inherit,
+    inheritMethods: inheritMethods
+  }
+
 }
 
-function attachMethod (chld, parnt, name) {
-  chld.prototype[name] = parnt.prototype[name];
-}
-
-function inheritMethods (child, parnt) {
-  var methods = Array.prototype.slice.call(arguments, 2);
-  if (!methods.length) throw 'Avoid calling this function with no methods to inherit';
-  methods.forEach(attachMethod.bind(null, child, parnt));
-  methods = null;
-  child = null;
-  parnt = null;
-}
-
-module.exports = {
-  inherit: inherit,
-  inheritMethods: inheritMethods
-};
+module.exports = createLib;
---
+++ test/test.js
@@ -0,0 +1,62 @@
+var expect = require('chai').expect,
+  checks = require('allex_checkslowlevellib'),
+  lib = require('..')(checks.has,checks.isFunction,checks.isString);
+
+describe('Testing \'Inherit\' lib', function(){
+  it('inherit', function(){
+    var chld = -1; 
+    var parnt = -1;
+    expect(lib.inherit.bind(lib,chld,parnt)).to.throw(Error,/is not a function/);
+
+    chld = function(){};
+    expect(lib.inherit.bind(lib,chld,parnt)).to.throw(Error,/is not a function/);
+
+    parnt = {};
+    expect(lib.inherit.bind(lib,chld,parnt)).to.throw(Error,/is not a function/);
+
+    parnt = new Object();
+    expect(lib.inherit.bind(lib,chld,parnt)).to.throw(Error,/is not a function/);
+
+    parnt = [];
+    expect(lib.inherit.bind(lib,chld,parnt)).to.throw(Error,/is not a function/);
+
+    function A () {
+      this.classname = 'A';
+    }
+    A.prototype.print = function () {
+      console.log(this);
+    };
+    function B () {
+      A.call(this);
+      this.classname = 'B';
+    }
+    expect(lib.inherit(B,A)).to.be.true;
+  });
+
+  it('inheritMethods', function(){
+    var chld = -1; 
+    var parnt = -1;
+    expect(lib.inheritMethods.bind(lib,chld,parnt)).to.throw(Error,/with no methods to inherit/);
+    var methodName = true;
+    expect(lib.inheritMethods.bind(lib,chld,parnt,methodName)).to.throw(Error,/is not a function/);
+    chld = function(){};
+    expect(lib.inheritMethods.bind(lib,chld,parnt,methodName)).to.throw(Error,/is not a function/);
+    parnt = function(){};
+    expect(lib.inheritMethods.bind(lib,chld,parnt,methodName)).to.throw(Error,/is not a string/);
+    methodName = 'print';
+    expect(lib.inheritMethods.bind(lib,chld,parnt,methodName)).to.throw(Error,/does not have method/);
+
+    function A () {
+      this.classname = 'A';
+    }
+    A.prototype.print = function () {
+      console.log(this);
+    };
+    function B () {
+      A.call(this);
+      this.classname = 'B';
+    }
+    expect(lib.inheritMethods(B,A,methodName)).to.be.true;
+  });
+
+});
