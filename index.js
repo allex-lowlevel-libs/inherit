e098fb7f6089682edb2c16eaa486947f58499def
Luka Petrovic <lukapetrovicsi@gmail.com>
2016-07-26 14:33:45 +0200

    Changed error messages.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function createLib(has,isFunction,isString){
   'use strict';
   function inherit (child, parnt) {
     if (!isFunction(child)){
-      throw new Error(child + ' is not a function.'); 
+      throw new Error('Child provided to "inherit" is not a function.'); 
     }
     if (!isFunction(parnt)){
-      throw new Error(parnt + ' is not a function.'); 
+      throw new Error('Parent provided to "inherit" is not a function.'); 
     }
     child.prototype = Object.create(parnt.prototype,{constructor:{
       value: child,
@@ -18,16 +18,16 @@ function createLib(has,isFunction,isString){
 
   function attachMethod (chld, parnt, name) {
     if (!isFunction(chld)){
-      throw new Error(chld + ' is not a function.'); 
+      throw new Error('Child provided to "inheritMethods" is not a function.'); 
     }
     if (!isFunction(parnt)){
-      throw new Error(parnt + ' is not a function.'); 
+      throw new Error('Parent provided to "inheritMethods" is not a function.'); 
     }
     if (!isString(name)){
-      throw new Error(name + ' is not a string. Method name must be a string.');
+      throw new Error(name + 'Method name provided to "inheritMethods" is not a string. Method name must be a string.');
     }
     if (!has(parnt.prototype,name)){
-      throw new Error('Parent class does not have method ' + name);
+      throw new Error('Parent class does not have method ' + name + '.');
     }
     chld.prototype[name] = parnt.prototype[name];
   }
